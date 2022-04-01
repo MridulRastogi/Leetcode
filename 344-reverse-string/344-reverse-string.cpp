a178cf8d49947cbab6bb33cf44718a7dedcb01e9
Mridul Rastogi <64559497+MridulRastogi@users.noreply.github.com>
2022-04-01 13:13:58 +0530

    Time: 28 ms (59.77%), Space: 23.2 MB (76.65%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -4,7 +4,7 @@ public:
         int l=s.size(), i;
         for(i=0; i<(l/2); i++)
             swap(s[i], s[l-i-1]);
-        for(i=0; i<l; i++)
+        for(i=l-1; i>=0; i--)
             cout << s[i];
     }
 };
