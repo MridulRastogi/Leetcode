a9c4f9a641a9bacd19287bbe8ffc403dbf382443
Mridul Rastogi <64559497+MridulRastogi@users.noreply.github.com>
2022-05-27 14:05:23 +0530

    Time: 675 ms (8.65%), Space: 306.9 MB (7.40%) - LeetHub

--- 648-replace-words/648-replace-words.cpp
+++ 648-replace-words/648-replace-words.cpp
@@ -17,38 +17,13 @@ public:
             else
             {
                 for(int j=0; j<dic.size(); j++)
-                {
                     if(dic[j]==str.substr(0, dic[j].length()))
                         str = dic[j];
-                }
                 s = s + str + " ";
                 str = "";
             }
         }
         s = s.substr(0,s.length()-1);
         return s;
-        /*
-        for(int i=0; i<sen.size(); i++)
-        {
-            int l = sen[i].length();
-            for(int j=0; j<dic.size(); j++)
-            {
-                if(dic[j].length()>=l)
-                {
-                    if(dic[j]==sen[i].substr(0, dic[j].length()))
-                    {
-                        string change = dic[j];
-                        swap(change, sen[i]);
-                        break;
-                    }
-                }
-            }
-        }
-        string s = "";
-        for(int i=0; i<sen.size(); i++)
-            if(i!=sen.size()-1)
-                s += sen[i] + " ";
-        return s;
-        */
     }
 };
