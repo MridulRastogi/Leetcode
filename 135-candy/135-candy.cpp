4925f81ddf27d1dcca5826f3a5ac85987af83558
Mridul Rastogi <64559497+MridulRastogi@users.noreply.github.com>
2022-07-05 11:13:11 +0530

    Time: 27 ms (74.11%), Space: 17.9 MB (41.00%) - LeetHub

---
+++ 135-candy/135-candy.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) 
+    {
+        int l = ratings.size();
+        vector<int> candies(l, 1);
+        for(int i=0; i<l-1; i++)
+            if(ratings[i+1] > ratings[i])
+                candies[i+1] = candies[i] + 1;
+        for(int i=l-1; i>0; i--)
+            if(ratings[i-1] > ratings[i])
+                if(candies[i-1] < (candies[i]+1))
+                    candies[i-1] = candies[i]+1;
+        return accumulate(candies.begin(), candies.end(), 0);
+    }
+};
