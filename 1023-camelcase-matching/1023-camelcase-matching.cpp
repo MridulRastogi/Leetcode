4522f30977d9fa562789af065535ea34e129cf10
Mridul Rastogi <64559497+MridulRastogi@users.noreply.github.com>
2022-08-06 23:05:29 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (100.00%) - LeetHub

--- 1023-camelcase-matching/1023-camelcase-matching.cpp
+++ 1023-camelcase-matching/1023-camelcase-matching.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     vector<bool> camelMatch(vector<string>& queries, string pattern) 
     {
-        int l = pattern.length(), c, count=0, lit;
-        vector<bool> res;
+        int l = pattern.length(), c;
         bool flag;
+        vector<bool> res;
         for(int i=0; i<queries.size(); i++)
         {
             c=0;
@@ -12,17 +12,11 @@ public:
             for(int j=0; j<queries[i].length(); j++)
             {
                 if(queries[i][j]==pattern[c] and c<l)
-                {
                     c++;
-                    lit = j;
-                }    
                 else if(queries[i][j]!=pattern[c] and isupper(queries[i][j]))
                     flag = false;
             }
-            if(flag == false or c!=l) 
-                res.push_back(false);
-            else if(flag == true and c==l)
-                res.push_back(true);
+            res.push_back((flag==true and c==l) ? true : false);
         }
         return res;
     }
