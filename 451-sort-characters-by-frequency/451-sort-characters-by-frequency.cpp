99684d1322362c0b8576270208575e349ce4ced4
Mridul Rastogi <64559497+MridulRastogi@users.noreply.github.com>
2022-08-16 23:19:22 +0530

    Time: 13 ms (80.53%), Space: 8.9 MB (13.79%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -1,28 +1,19 @@
-class Solution {
-public:
+bool cmpSizes(const string &lhs, const string &rhs) 
+{
+    return lhs.length() > rhs.length();
+}
+class Solution 
+{
+    public:
     string frequencySort(string s) 
     {
-        string s2 = "";
-        map<char, int> m;
-        vector<pair<int, char>> vec;
-        
+        vector<string> vec(75, "");
         for(int i=0; i<s.length(); i++)
-        {
-            if(m.find(s[i])==m.end())
-                m[s[i]] = 1;
-            else 
-                m[s[i]]++;
-        }
-        
-        for(auto it:m)
-            vec.push_back(make_pair(it.second, it.first));
-        
-        sort(vec.begin(), vec.end());
-        
-        for(int i=vec.size()-1; i>=0; i--)
-            for(int j=0; j<vec[i].first; j++)
-                s2 += vec[i].second;
-       
-        return s2;
+            vec[s[i]-'0'] += s[i];
+        sort(vec.begin(), vec.end(), cmpSizes);
+        s = "";
+        for(string str:vec)
+            s += str;
+        return s;
     }
 };
