6d0f3fff17b4ad07d601337887643628090b4a3e
Mridul Rastogi <64559497+MridulRastogi@users.noreply.github.com>
2022-08-25 16:38:22 +0530

    Time: 5 ms (74.47%), Space: 46 MB (66.46%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -3,10 +3,17 @@ class Solution {
     {
         int[] r = new int[26];
         int[] m = new int[26];
-        for(int i=0; i<ransomNote.length(); i++)
-            r[ransomNote.charAt(i)-'0'-49]++;
-        for(int i=0; i<magazine.length(); i++)
-            m[magazine.charAt(i)-'0'-49]++;
+        
+        int rl = ransomNote.length();
+        int ml = magazine.length();
+        int maxi = Math.max(ransomNote.length(), magazine.length());
+        for(int i=0; i<maxi; i++)
+        {
+            if(i<rl)
+                r[ransomNote.charAt(i)-'0'-49]++;
+            if(i<ml)
+                m[magazine.charAt(i)-'0'-49]++;    
+        }
         for(int i=0; i<26; i++)
             if(m[i]<r[i])
                 return false;
