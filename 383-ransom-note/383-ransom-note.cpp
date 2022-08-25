fffc4394570dccd96334778861b2628156aa5a95
Mridul Rastogi <64559497+MridulRastogi@users.noreply.github.com>
2022-08-25 16:09:47 +0530

    Time: 37 ms (31.12%), Space: 9.1 MB (5.09%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) 
+    {
+        map<char, int> r,m;
+        for(char ch:ransomNote)
+            r[ch]++;
+        for(char ch:magazine)
+            m[ch]++;
+        for(auto it:r)
+        {
+            if(m.find(it.first) == m.end())
+                return false;
+            else 
+            {
+                auto pos = m.find(it.first);
+                if(pos->second < it.second)
+                    return false;
+            }
+        }
+        return true;
+    }
+};
