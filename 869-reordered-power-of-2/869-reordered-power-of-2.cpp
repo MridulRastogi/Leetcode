c70b514f52b5155138b7f207d2a516be2afeb692
Mridul Rastogi <64559497+MridulRastogi@users.noreply.github.com>
2022-08-26 23:20:03 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (19.77%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,34 +1,31 @@
-class Solution {
-public:
+class Solution 
+{
+    public:
     bool reorderedPowerOf2(int n) 
     {
         vector<string> vec;
         string s = to_string(n);
-        long res=1, c=1;
-        int maxi = pow(10,s.length());
+        int res=1, c=1, maxi = pow(10, s.length());
         
         while(res <= maxi)
         {
             vec.push_back(to_string(res));  
-            res = pow(2,c);
-            c++;
+            res = pow(2, c++);
         }
         
         map<char, int> sm;
         for(int i=0; i<s.length(); i++)
             sm[s[i]]++;
         
-        for(string str:vec)
+        for(string str : vec)
         {
-            map<char, int> vm;
-            for(int i=0; i<str.length(); i++)
-                vm[str[i]]++;
             if(s.length() == str.length())
             {
+                map<char, int> vm;
+                for(int i=0; i<str.length(); i++)
+                    vm[str[i]]++;
                 if(vm==sm)
-                {
                     return true;
-                }
             }
         }
         return false;        
