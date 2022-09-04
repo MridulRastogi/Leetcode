208d92e66957d935ae886122bc1dd3f5f23630f2
Mridul Rastogi <64559497+MridulRastogi@users.noreply.github.com>
2022-09-04 18:05:09 +0530

    Time: 10 ms (43.88%), Space: 7.6 MB (47.67%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,16 +1,14 @@
-class Solution {
-public:
+class Solution 
+{
+    public:
     bool isPalindrome(string s) 
     {
-        if(s==" ")
-            return true;
         string r="";
         for(int i=0; i<s.length(); i++)
             if((s[i]>=65 and s[i]<=90) or (s[i]>=97 and s[i]<=122) or (s[i]>=48 and s[i]<=57))
                 r += tolower(s[i]);
         s = r;
         reverse(r.begin(), r.end());
-        cout << s << endl << r << endl;
         return (s==r);
     }
 };
