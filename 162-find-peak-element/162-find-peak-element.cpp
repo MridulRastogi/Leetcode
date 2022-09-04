c6e25e750482cc5c770c30981fd8335ed4ae54dd
Mridul Rastogi <64559497+MridulRastogi@users.noreply.github.com>
2022-09-04 23:23:07 +0530

    Time: 7 ms (52.77%), Space: 8.9 MB (51.42%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -5,12 +5,17 @@ public:
         if(nums.size()==1) 
             return 0;
         int n = nums.size();
-        int pos = (nums[0]>nums[1] ? 0 : (nums[n-1]>nums[n-2] ? n-1 : 0));
-        if(nums[0]>nums[1])
-            pos = 0;
-        for(int i=1; i<n-1; i++)
-            if(nums[i]>nums[i-1] and nums[i]>nums[i+1])
-                return i;
-        return pos;
+        int l=1, r=n-2;
+        int pos = (nums[0]>nums[1] ? 0 : (nums[n-1]>nums[n-2] ? n-1 : -1));
+        while(l<=r)
+        {
+            if(nums[l]>nums[l-1] and nums[l]>nums[l+1])
+                return l;
+            if(nums[r]>nums[r-1] and nums[r]>nums[r+1])
+                return r;
+            l++;
+            r--;
+        }
+        return (pos==-1 ? 0 : pos);
     }
 };
