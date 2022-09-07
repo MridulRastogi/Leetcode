35383680641c191cadfdd5eba33466011788be08
Mridul Rastogi <64559497+MridulRastogi@users.noreply.github.com>
2022-09-07 12:26:18 +0530

    Time: 2944 ms (5.02%), Space: 6 MB (46.14%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,18 +1,31 @@
-class Solution {
-public:
+class Solution 
+{
+    public:
     double myPow(double x, int n) 
     {
-        return pow(x,n);
-//         double res = 0;
-//         double num = x;
-//         int power = n;
+        double number = x, power = n;
 
-//         if(power == 0)
-//             return 1;
-//         else if(num == 1)
-//             return 1;
-//         else 
-//             res = pow(x, double(n));
-//         return res;
+        if(power == 0 or number == 1)
+            return 1;
+        else if(abs(number)==1)
+        {
+            if(abs(power) == power)
+                return number;
+            else
+                return abs(number);
+        }
+        else 
+        {
+            if(n==INT_MIN)
+                return 0;
+            double result = x;
+            int value = int(abs(n));
+            for(int i=1; i<value; i++)
+                result = result*x;
+            if(n<0)
+                result = 1/result;
+            return result;
+        }
+        return 0;
     }
 };
