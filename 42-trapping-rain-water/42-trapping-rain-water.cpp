79d4a33c804b81cc54c97f33a654768d7e81c67d
Mridul Rastogi <64559497+MridulRastogi@users.noreply.github.com>
2022-09-18 12:40:32 +0530

    Time: 43 ms (9.88%), Space: 20.8 MB (7.91%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,20 +1,25 @@
-class Solution {
-public:
-    int trap(vector<int>& height) 
+class Solution 
+{
+    public:
+    int trap(vector<int>& height)       // 0 1 0 2 1 0 1 3 2 1 2 1
     {
         vector<int> rightMax;
         int cost=0, leftMax=height[0], rm=INT_MIN;
-        for(int i=height.size()-1; i>=0; i--)
+        
+        for(int i=height.size()-1; i>=0; i--)       // taking all the maximum heights from the right end of the vector
         {
-            rm = max(height[i], rm);
-            rightMax.push_back(rm);
+            rm = max(height[i], rm);                // according to this values obtained through rm will be as 1 2 3
+            rightMax.push_back(rm);                 // thus the final state of rm will be {1,2,3}
         }
-        reverse(rightMax.begin(), rightMax.end());
-        for(int i=1; i<height.size()-1; i++)
+        
+        reverse(rightMax.begin(), rightMax.end());      // reversing the rm vector to obtain {3,2,1} for 
+                                        
+        for(int i=1; i<height.size()-1; i++)            // considering the elements obtained from leftMax are {1,2,3}
         {
             leftMax = max(leftMax, height[i]);
             cost += min(leftMax, rightMax[i])-height[i];
         }
+        
         return cost;
     }
 };
