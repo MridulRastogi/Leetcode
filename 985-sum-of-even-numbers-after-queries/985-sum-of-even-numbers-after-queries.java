a48f440aedd28b9ea37cd5c228be798bd58a891c
Mridul Rastogi <64559497+MridulRastogi@users.noreply.github.com>
2022-09-21 20:28:34 +0530

    Time: 14 ms (14.15%), Space: 66.7 MB (87.78%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
@@ -4,7 +4,7 @@ class Solution
     {
         int s = 0;
         for(int i=0; i<nums.length; i++)
-            if(Math.abs(nums[i])%2==0)
+            if((nums[i]&1) == 0)
                 s += nums[i];
         return s;
     }
@@ -21,19 +21,19 @@ class Solution
             nums[queries[i][1]] += value;
             current              = nums[queries[i][1]];
 
-            if(recent%2 == 0)
+            if((recent&1)==0)
             {
-                if(current%2 == 0)
+                if((current&1) == 0)
                 {
                     if(recent > current)
                         res -= Math.abs(value);
                     else if(recent < current)
                         res += Math.abs(value);
                 }
-                else if(Math.abs(current)%2 == 1)
+                else if((Math.abs(current)&1)==1)
                     res -= recent;
             }
-            else if((Math.abs(recent)%2)==1 && (Math.abs(current)%2)==0)
+            else if( (Math.abs(recent)&1)==1 && (Math.abs(current)&1)==0 )
                 res = res + current;
             vec[i] = res;    
         }
