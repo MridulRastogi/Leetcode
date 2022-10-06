cb3434c35864a01f668e162bfc9888c387c164b4
Mridul Rastogi <64559497+MridulRastogi@users.noreply.github.com>
2022-10-06 17:33:58 +0530

    Time: 1 ms (98.32%), Space: 40.4 MB (83.67%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -1,15 +1,16 @@
-class Solution {
+class Solution 
+{
     public boolean isHappy(int n) 
     {
-        int r, s=0;
+        int r,s=0;
         while(n>9 || n==7)
         {
             s=0;
             while(n>0)
             {
-                r = n%10;
-                s += Math.pow(r,2);
-                n = n/10;
+                r=n%10;
+                s+=Math.pow(r,2);
+                n/=10;
             }
             n=s;
         }
