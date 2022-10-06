1c93b784c7027d72832fc32ddd2c68f8f2796ff5
Mridul Rastogi <64559497+MridulRastogi@users.noreply.github.com>
2022-10-06 17:35:53 +0530

    Time: 9 ms (10.78%), Space: 5.9 MB (63.16%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -3,18 +3,17 @@ class Solution
     public:
     bool isHappy(int n) 
     {
-        int num = n, r, s=0;
-        while(num>9 or num==7)
+        int s=0;
+        while(n>9 or n==7)
         {
-            s = 0;
-            while(num>0)
+            s=0;
+            while(n>0)
             {
-                r = num%10;
-                s += pow(r,2);
-                num /= 10; 
+                s=s+pow(n%10,2);
+                n=n/10; 
             }
-            num = s;
+            n=s;
         }
-        return (num==1);
+        return (n==1);
     }
 };
