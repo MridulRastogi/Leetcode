82d303239921c9ef968c94e1de456e418fb3f2f0
Mridul Rastogi <64559497+MridulRastogi@users.noreply.github.com>
2022-10-08 23:44:39 +0530

    Time: 69 ms (80.08%), Space: 29.8 MB (39.64%) - LeetHub

--- 962-maximum-width-ramp/962-maximum-width-ramp.cpp
+++ 962-maximum-width-ramp/962-maximum-width-ramp.cpp
@@ -1,13 +1,16 @@
+#define all(vec) vec.begin(),vec.end()
+#define mp(a,b) make_pair(a,b)
+#define comparator greater<pair<int, int>>()
 class Solution 
 {
     public:
-    int getPos(vector<pair<int, int>>& vec, int num)
-    {
-        for(int i=0; i<vec.size(); i++)
-            if(vec[i].first <= num)
-                return vec[i].second;
-        return 0;
-    }
+    // int getPos(vector<pair<int, int>>& vec, int num)
+    // {
+    //     for(int i=0; i<vec.size(); i++)
+    //         if(vec[i].first <= num)
+    //             return vec[i].second;
+    //     return 0;
+    // }
     int maxWidthRamp(vector<int>& nums) 
     {
         vector<pair<int, int>> vec;
@@ -19,8 +22,7 @@ class Solution
             else
             {
                 //pos = getPos(vec, nums[i]);
-                                pos = lower_bound(vec.begin(), vec.end(), make_pair(nums[i],INT_MAX), greater<pair<int, int>>())->second;
-
+                pos = lower_bound(all(vec), mp(nums[i],INT_MAX), comparator)->second;
                 result = max(result, i-pos);
             }
         }
