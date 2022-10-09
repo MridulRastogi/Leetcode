2f0f6859d9900a5e862c57d2546894941a2271f0
Mridul Rastogi <64559497+MridulRastogi@users.noreply.github.com>
2022-10-09 11:33:08 +0530

    Time: 77 ms (50.73%), Space: 35.9 MB (98.68%) - LeetHub

---
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,56 @@
+
+class Solution 
+{
+    public:
+    TreeNode* head;
+    bool res = false, res2 = false;
+    
+    void search(TreeNode* root, TreeNode* z, int value)
+    {
+        if(root == NULL)
+            return;
+        if(root->val == value and root!=z)
+        {
+            res2 = true;
+            return;
+        }
+        search(root->left, z, value);
+        search(root->right, z, value);
+    }
+    
+    void preorder(TreeNode* root, int k)
+    {
+        if(root == NULL)
+            return;
+        //if(root->val < k)
+        //{
+            search(head, root, k-(root->val));
+            if(res2 == true)
+            {
+                res = true;
+                return;
+            }
+        //}
+        preorder(root->left, k);
+        preorder(root->right, k);
+    }
+    
+    bool findTarget(TreeNode* root, int k) 
+    {
+        head = root;
+        preorder(root, k);       
+        return res;
+    }
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
