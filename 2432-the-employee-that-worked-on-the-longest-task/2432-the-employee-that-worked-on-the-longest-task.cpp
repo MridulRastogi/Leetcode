ac20e4384fe35118759ebbbe07868f217f01d24c
Mridul Rastogi <64559497+MridulRastogi@users.noreply.github.com>
2022-10-09 12:59:04 +0530

    Time: 194 ms (25.00%), Space: 37.8 MB (25.00%) - LeetHub

--- 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.cpp
+++ 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.cpp
@@ -8,7 +8,9 @@ class Solution
 
         vec.push_back(make_pair(logs[0][0], logs[0][1]));
         for(int i=1; i<logs.size(); i++)
+        {
             vec.push_back(make_pair(logs[i][0], logs[i][1]-logs[i-1][1]));
+        }
         sort(vec.begin(), vec.end());
         
         for(int i=0; i<vec.size(); i++)
