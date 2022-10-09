b42ad5a16a3a6301703d58161b97de4f7bb7ada5
Mridul Rastogi <64559497+MridulRastogi@users.noreply.github.com>
2022-10-09 13:01:44 +0530

    Time: 193 ms (25.00%), Space: 39.4 MB (25.00%) - LeetHub

--- 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.cpp
+++ 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.cpp
@@ -3,22 +3,19 @@ class Solution
     public:
     int hardestWorker(int n, vector<vector<int>>& logs) 
     {
-        vector<pair<int, int>> vec;
+        map<int, int> mp;
         int maxi = 0, eid;
-
-        vec.push_back(make_pair(logs[0][0], logs[0][1]));
-        for(int i=1; i<logs.size(); i++)
-        {
-            vec.push_back(make_pair(logs[i][0], logs[i][1]-logs[i-1][1]));
-        }
-        sort(vec.begin(), vec.end());
         
-        for(int i=0; i<vec.size(); i++)
+        mp[logs[0][0]] = logs[0][1];
+        for(int i=1; i<logs.size(); i++)
+            mp[logs[i][0]] = max( mp[logs[i][0]] , logs[i][1]-logs[i-1][1] );
+        
+        for(auto it : mp)
         {
-            if(maxi < vec[i].second)
+            if(maxi < it.second)
             {
-                maxi = vec[i].second;
-                eid  = vec[i].first;
+                maxi = it.second;
+                eid  = it.first;
             }
         }
         return eid;
