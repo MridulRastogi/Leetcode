e36557543a7e7751feda2c6eb2ef5cba95c2b9f6
Mridul Rastogi <64559497+MridulRastogi@users.noreply.github.com>
2022-10-12 16:12:28 +0530

    Time: 61 ms (58.84%), Space: 21.9 MB (22.88%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -1,15 +1,11 @@
-class Solution 
-{
+class Solution {
     public:
-    int largestPerimeter(vector<int>& nums) 
-    {
+    int largestPerimeter(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int l = nums.size();
         for(int i=l-1; i>1; i--)
-        {
-            if(nums[i-1]+nums[i-2] > nums[i])
+            if((nums[i-1]+nums[i-2]) > nums[i])
                 return nums[i]+nums[i-1]+nums[i-2];
-        }
         return 0;
     }
 };
