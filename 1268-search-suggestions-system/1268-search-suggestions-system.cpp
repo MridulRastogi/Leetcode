a45937a9ea2ef9f82c92a7e2005ba5c25d606e8e
Mridul Rastogi <64559497+MridulRastogi@users.noreply.github.com>
2022-10-14 00:16:16 +0530

    Time: 72 ms (86.42%), Space: 34 MB (64.31%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -4,24 +4,20 @@ public:
     {
         sort(products.begin(), products.end());
         vector<vector<string>> vec;
-        int count=0, i, j=0, l=products.size();
+        int count, i, j, l=products.size();
         string fetch;
         for(i=1; i<=s.length(); i++)
         {
-            fetch = s.substr(0,i);
             vector<string> v;
-            count = 0;
-            j = 0;
+            count=0, j=0, fetch=s.substr(0,i);
             while(j<l and count<3)
             {
                 if(products[j].length() >= i)
-                {
                     if(products[j].substr(0,i) == fetch)
                     {
                         v.push_back(products[j]);
                         count++;
                     }
-                }
                 j++;
             }
             vec.push_back(v);
