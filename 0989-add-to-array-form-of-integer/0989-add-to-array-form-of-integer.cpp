f98c8df1bf8d71c0d22c7ed5b43eacd029c47587
Mridul Rastogi <64559497+MridulRastogi@users.noreply.github.com>
2022-10-14 21:44:36 +0530

    Time: 646 ms (5.04%), Space: 438.2 MB (5.09%) - LeetHub

---
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
@@ -0,0 +1,53 @@
+#define all(x) x.begin(),x.end()
+class Solution 
+{
+    public:
+    string cvts(vector<int>& vec)
+    {
+        string res = "";
+        for(int i=0; i<vec.size(); i++)
+            res += to_string(vec[i]);
+        return res;
+    }
+    string process(string a, string b)
+    {
+        reverse(all(a));
+        reverse(all(b));
+        int G = max(a.length(), b.length());
+        int L = min(a.length(), b.length());
+        if(a.length() < b.length())
+            swap(a,b);
+        
+        int sum, carry=0;
+        string result = "";
+        for(int i=0; i<G; i++)
+        {
+            if(i>=L)
+                sum = (a[i]-'0') + carry;
+            else
+                sum = (a[i]-'0') + (b[i]-'0') + carry;
+            
+            carry = (sum>9 ? sum/10 : 0);
+            sum    = sum%10;
+            result = to_string(sum) + result;
+        }
+        if(carry > 0)
+            result = to_string(carry) + result;
+        return result;
+    }
+    vector<int> to_vector(string result)
+    {
+        vector<int> res;
+        for(int i=0; i<result.length(); i++)
+            res.push_back(result[i]-'0');
+        return res;
+    }
+    vector<int> addToArrayForm(vector<int>& num, int k) 
+    {
+        string a = cvts(num);
+        string b = to_string(k);
+        string r = process(a,b);
+        vector<int> result = to_vector(r);
+        return result;
+    }
+};
