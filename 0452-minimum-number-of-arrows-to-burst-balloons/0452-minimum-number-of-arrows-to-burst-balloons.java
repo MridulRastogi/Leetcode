ea519deac3b13cc783e8ea3cf12a34791087dddd
Mridul Rastogi <64559497+MridulRastogi@users.noreply.github.com>
2022-10-30 17:49:19 +0530

    Time: 118 ms (63.38%), Space: 96.2 MB (61.77%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
@@ -1,19 +1,13 @@
 class Solution 
-{
-    static void columnWiseSorting(int points[][]) 
+{  
+    public int findMinArrowShots(int[][] points) 
     {
-        Arrays.sort(points, new Comparator<int[]>() 
-                        {
+        Arrays.sort(points, new Comparator<int[]>() {
                             public int compare(int[] x, int[] y) 
                             {
                                 return (x[1] > y[1] ? 1 : -1);
                             }
-                        }
-                   );
-    }   
-    public int findMinArrowShots(int[][] points) 
-    {
-        columnWiseSorting(points);
+                        });
         int val = points[0][1], result=1;
         for(int i=0; i<points.length; i++)
         {
