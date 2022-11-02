a7151de152754b9aca651b9c1100c9daea89ea68
Mridul Rastogi <64559497+MridulRastogi@users.noreply.github.com>
2022-11-02 17:12:48 +0530

    Time: 4 ms (29.93%), Space: 6.6 MB (81.49%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
@@ -1,15 +1,33 @@
 class Solution 
 {
     public:
+    void process(string& s, vector<char>& choices, unordered_set<string>& banks, unordered_set<string>& visited, queue<string>& collection)
+    {
+        for(int i=0; i<8; i++)
+        {
+            char ch = s[i];
+            for(char letter:choices)
+            {
+                s[i] = letter;
+                if(banks.count(s) and !visited.count(s))
+                {
+                    collection.push(s);
+                    visited.insert(s);
+                }
+            }
+            s[i] = ch;
+        }
+    }
+    
     int minMutation(string start, string end, vector<string>& bank) 
     {
         vector<char> choices = {'A', 'C', 'G', 'T'};
         unordered_set<string> banks, visited;
         queue<string> collection;
-        int result = 0, len;
         string s;
         char ch;
-        
+        int result = 0, len;
+
         for(string s:bank)
             banks.insert(s);
         if(!banks.count(end))
@@ -25,28 +43,12 @@ class Solution
             {
                 s = collection.front();
                 collection.pop();
-               
                 if(s == end)
                     return result;             
-                
-                for(int i=0; i<8; i++)
-                {
-                    ch = s[i];
-                    for(char letter:choices)
-                    {
-                        s[i] = letter;
-                        if(banks.count(s) and !visited.count(s))
-                        {
-                            collection.push(s);
-                            visited.insert(s);
-                        }
-                    }
-                    s[i] = ch;
-                }
+                process(s, choices, banks, visited, collection);
             }
             result++;
         }
-        
         return -1;
     }
 };
