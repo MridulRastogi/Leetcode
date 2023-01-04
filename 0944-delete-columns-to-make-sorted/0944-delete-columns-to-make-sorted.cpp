31c428a8a2ede5533203dab68add352237a71677
Mridul Rastogi <64559497+MridulRastogi@users.noreply.github.com>
2023-01-04 18:27:45 +0530

    Time: 107 ms (42.80%), Space: 12.2 MB (30.27%) - LeetHub

---
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -0,0 +1,22 @@
+class Solution 
+{
+    public:
+    int minDeletionSize(vector<string>& strs) 
+    {
+        int n = strs[0].length(), c=0;
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<strs.size()-1; j++)
+            {
+                if(strs[j][i] <= strs[j+1][i])
+                    continue;
+                else
+                {
+                    c++;
+                    break;
+                }
+            }
+        }
+        return c;
+    }
+};
